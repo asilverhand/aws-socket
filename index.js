8b0ce82d2cd6e66455feadce9d87c33a3a5c33a7
Allan Santiago <allan75imp@gmail.com>
2023-10-28 19:43:59 -0300

    feat: new client route

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const io = require('socket.io')(server, { cors: { origin: '*' } })
 
 const PORT = 3001
 
+app.get("/", (req, res, next) => {
+  res.json({
+    count: io.eio.clientsCount
+  });
+});
+
+
+
 io.on('connection', socket => {
   console.log('Usuário conectado!', socket.id);
-
   socket.on('disconnect', reason => {
     console.log('Usuário desconectado!', socket.id)
   })
 
   socket.on('set_username', username => {
     socket.data.username = username
+    console.log('user_name', socket.data)
   })
 
   socket.on('message', text => {
@@ -22,6 +30,12 @@ io.on('connection', socket => {
       author: socket.data.username
     })
   })
+
+  setInterval(() => {
+    io.emit('users', {
+      count: io.eio.clientsCount
+    })
+  }, 5000);
 })
 
 server.listen(PORT, () => console.log('Server running...'))
